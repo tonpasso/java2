f7fa62472c55fcff1c3676c934e654412e8cbdc4
ericmoraess <ericmoraess@gmail.com>
2020-05-16 15:01:07 -0300

    criando entidade beneficiario.

---
+++ ConexaoBancoDeDados/src/main/java/br/com/ericmoraess/entidade/Beneficiario.java
@@ -0,0 +1,13 @@
+package br.com.ericmoraess.entidade;
+
+public class Beneficiario {
+	private String NIS;
+
+	public String getNIS() {
+		return NIS;
+	}
+
+	public void setNIS(String nIS) {
+		NIS = nIS;
+	}
+}
