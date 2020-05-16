b55c99fb44ecd1e72708014999d03bb49d91777f
ericmoraess <ericmoraess@gmail.com>
2020-05-16 15:20:17 -0300

    criando método responsável por inserir um hospital.

--- ConexaoBancoDeDados/src/main/java/br/com/ericmoraess/dao/HospitalDAOImp.java
+++ ConexaoBancoDeDados/src/main/java/br/com/ericmoraess/dao/HospitalDAOImp.java
@@ -39,7 +39,7 @@ public class HospitalDAOImp implements HospitalDAO {
 	}
 
 	public boolean insertHospital() {
-		// TODO Auto-generated method stub
+		System.out.println("inserir hospital");
 		return false;
 	}
 
