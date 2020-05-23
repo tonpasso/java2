fb7e9d8e472927a7cbc749bac81c2e56364a3d04
tonpa <wellpasso@gmail.com>
2020-05-23 15:12:27 -0300

    Solicitação para alterar

--- ConexaoBancoDeDados/src/main/java/br/com/ericmoraess/dao/HospitalDAOImp.java
+++ ConexaoBancoDeDados/src/main/java/br/com/ericmoraess/dao/HospitalDAOImp.java
@@ -40,6 +40,7 @@ public class HospitalDAOImp implements HospitalDAO {
 
 	public boolean insertHospital() {
 		// TODO Auto-generated method stub
+		System.out.println("Well");
 		return false;
 	}
 
